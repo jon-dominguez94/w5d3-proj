ed90ad734bf2a1692a2d7bfa39104d2f05eb560b
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-21 14:23:00 -0800

    Write already_built response?

--- skeleton/lib/controller_base.rb
+++ skeleton/lib/controller_base.rb
@@ -14,6 +14,7 @@ class ControllerBase
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    !!@already_built_response
   end
 
   # Set the response status code and header
@@ -27,11 +28,19 @@ class ControllerBase
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise if already_built_response?
+    @res['Content-Type'] = content_type
+    @res.write(content)
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    path = File.dirname(__FILE__)
+    new_path = File.join(path, 'views', "#{template_name}.html_safe")
+
+
   end
 
   # method exposing a `Session` object
